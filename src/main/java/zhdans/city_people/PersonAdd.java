fc86e8487a2f4aef416de424e4c5e4ef8062d0b0
Solodovnikov Zhdan <zhdansolodovnikov@gmail.com>
2024-02-03 14:12:41 +0600

    change info and update by number

--- src/main/java/zhdans/city_people/PersonAdd.java
+++ src/main/java/zhdans/city_people/PersonAdd.java
@@ -10,22 +10,44 @@ import java.util.Scanner;
 
 public class PersonAdd {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите имя человека: ");
-        String name = scanner.nextLine();
-
-        System.out.print("\nВведите id города: ");
-        int city_id = Integer.parseInt(scanner.nextLine());
-
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
         EntityManager manager = factory.createEntityManager();
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1) Изменить существующего человека\n2) Создать нового человека\nВыберите действие: ");
+        int num = Integer.parseInt(scanner.nextLine());
+
+        String name = "";
+        City city = new City();
+        Person person = new Person();
+
+        if (num == 1){
+            System.out.print("\nВведите id человека: ");
+            String person_id = scanner.nextLine();
+
+            System.out.print("Введите имя человека: ");
+            name = scanner.nextLine();
+
+            System.out.print("Введите id города: ");
+            int city_id = Integer.parseInt(scanner.nextLine());
+
+            city = manager.find(City.class, city_id);
+            person = manager.find(Person.class, person_id);
+
+        }else if (num == 2){
+            System.out.print("\nВведите имя человека: ");
+            name = scanner.nextLine();
+
+            System.out.print("Введите id города: ");
+            int city_id = Integer.parseInt(scanner.nextLine());
+
+            city = manager.find(City.class, city_id);
+            person = new Person();
+
+        }
+
         try {
             manager.getTransaction().begin();
-
-            City city = manager.find(City.class, city_id);
-            Person person = new Person();
             person.setName(name);
             person.setCity(city);
 
