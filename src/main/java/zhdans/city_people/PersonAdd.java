e8c71b2df6a8ea6c7fe0abf2829401c5ef31c920
Solodovnikov Zhdan <zhdansolodovnikov@gmail.com>
2024-02-03 13:44:12 +0600

    PersonAdd class add

--- src/main/java/zhdans/city_people/PersonAdd.java
+++ src/main/java/zhdans/city_people/PersonAdd.java
@@ -1,4 +1,41 @@
 package zhdans.city_people;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import zhdans.city_people.entity.City;
+import zhdans.city_people.entity.Person;
+
+import java.util.Scanner;
+
 public class PersonAdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите имя человека: ");
+        String name = scanner.nextLine();
+
+        System.out.print("\nВведите id города: ");
+        int city_id = Integer.parseInt(scanner.nextLine());
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("main");
+        EntityManager manager = factory.createEntityManager();
+
+        try {
+            manager.getTransaction().begin();
+
+            City city = manager.find(City.class, city_id);
+            Person person = new Person();
+            person.setName(name);
+            person.setCity(city);
+
+            manager.persist(person);
+            manager.getTransaction().commit();
+        }catch (Exception e){
+            manager.getTransaction().rollback();
+        }
+        manager.close();
+        factory.close();
+    }
+
 }
